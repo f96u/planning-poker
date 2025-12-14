e704a54935c4ef87543b23f285e11a792649c352
f96u <gtfukui@gmail.com>
2025-12-14 18:08:48 +0900

    change: APIを直接記入に変更

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyABsKPq5nJEfmBOZsXDUxojSDCRv_IxLBI",
+  authDomain: "planning-poker-de9bd.firebaseapp.com",
+  databaseURL: "https://planning-poker-de9bd-default-rtdb.firebaseio.com",
+  projectId: "planning-poker-de9bd",
+  storageBucket: "planning-poker-de9bd.firebasestorage.app",
+  messagingSenderId: "668964102108",
+  appId: "1:668964102108:web:05724f2445a8a3bd693919",
+  measurementId: "G-01P3EN9C4T"
 };
 
 // Initialize Firebase
