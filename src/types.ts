e814a6a63d37021dbbc10879260a90a44b903f45
f96u <gtfukui@gmail.com>
2025-12-19 23:40:04 +0900

    add: 非参加モードを追加

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ export interface User {
   name: string;
   vote: number | string | null; // 数値、'?'、または未投票(null)
   online: boolean;
+  isObserver?: boolean; // オブザーバーかどうか（デフォルト: false、つまり参加者）
 }
 
 // ルームの型
