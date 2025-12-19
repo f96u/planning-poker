b810a4bc56bf5a7823e72e0576a4b1295a2e93b7
f96u <gtfukui@gmail.com>
2025-12-19 09:04:55 +0900

    add: 部屋を定期的にクリアにするFunctionを追加

---
+++ functions/src/index.ts
@@ -0,0 +1,44 @@
+import {setGlobalOptions} from "firebase-functions";
+import {onRequest} from "firebase-functions/https";
+import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+import { onSchedule } from "firebase-functions/scheduler";
+
+setGlobalOptions({ maxInstances: 10 });
+
+admin.initializeApp();
+
+export const cleanupOldRooms = onSchedule(
+  {
+    schedule: 'every day 04:00',
+    timeZone: 'Asia/Tokyo',
+  },
+  async (event) => {
+    const db = admin.database();
+    const roomsRef = db.ref('rooms');
+
+    // 24時間以上経過したルームを削除
+    const now = Date.now();
+    const cutoff = now - 24 * 60 * 60 * 1000;
+    try {
+      const snapshot = await roomsRef
+        .orderByChild('createdAt')
+        .endAt(cutoff)
+        .once('value');
+      if (!snapshot.exists()) {
+        logger.info('削除対象の古い部屋はありませんでした。')
+        return;
+      }
+      const updates: { [key: string]: null } = {};
+      snapshot.forEach(child => {
+        updates[child.key] = null;
+      })
+
+      await roomsRef.update(updates);
+      logger.info(`${Object.keys(updates).length}件の古い部屋を削除しました。`)
+    } catch (error) {
+      logger.error('古い部屋の削除中にエラーが発生しました。', error);
+    }
+  }
+)
+
